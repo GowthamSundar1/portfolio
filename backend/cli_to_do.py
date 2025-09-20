87e5bd1feb96e61159c339cd16457a3bf47e03cd
GowthamSundar1 <gowtham.sundar1947@gmail.com>
2025-09-20 05:54:32 +0530

    first commit

--- backend/cli_to_do.py
+++ backend/cli_to_do.py
@@ -1,1 +1,16 @@
 #Started 17/09/2025
+import argparse
+import datetime
+
+parser = argparse.ArgumentParser(prog= "To-do List:-",
+                                 description= "This simple CLI lets you organise your tasks better")
+
+#initialize git
+
+#Add first task with id, desc, status, create_date, update_date
+
+#List tasks
+
+#update task using id and update status and update_date
+
+#List task that are done / not done/ in-progress
