0919fa11067e7ed33eb9026bda2d2d690225e2bb
GowthamSundar1 <gowtham.sundar1947@gmail.com>
2025-09-24 18:36:35 +0530

    commiting code changes for app

--- backend/cli_to_do.py
+++ backend/cli_to_do.py
@@ -3,36 +3,59 @@ import argparse
 from to_do_helper import *
 
 
-parser = argparse.ArgumentParser(prog= "To-do List:",
-                                 description= "This simple CLI app lets you organise your tasks better",
-                                 epilog="Thanks for trying it out!")
+class Main:
 
-subparser = parser.add_subparsers(dest="option", description="Describes the options of the to-do app")
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(prog= "To-do List:",
+                                         description= "This simple CLI app lets you organise your tasks better",
+                                         epilog="Thanks for trying it out!")
 
-add_task = subparser.add_parser("add", help="Adds a new task")
-add_task.add_argument(dest="task", type= str, metavar= "TASK", help= "Enter task to be added")
-add_task.add_argument("-s", "--status", dest="status", type= str, metavar= "STATUS", default="To-do")
-add_task.set_defaults(func=add_tasks)
+        self.subparser = self.parser.add_subparsers(dest="option", description="Describes the options of the to-do app")
 
-#done:List tasks
-list_task = subparser.add_parser("list", help="Lists tasks present in the application.")
-list_task.add_argument(dest="list", type= str, metavar="LIST", help= "Lists the tasks",
-                       choices=["all", "in-progress", "done", "not-done"])
+    def start_app(self):
+        """
+        Method to start the to do application functionalities
+        :return:
+        """
+        add_task = self.subparser.add_parser("add", help="Adds a new task")
+        add_task.add_argument(dest="task", type= str, metavar= "TASK", help= "Enter task to be added")
+        add_task.add_argument("-s", "--status", dest="status", type= str, metavar= "STATUS", default="To-do")
+        add_task.set_defaults(func=add_tasks)
 
+        #done:List tasks
+        #done:List task that are done / not done/ in-progress
+        list_task = self.subparser.add_parser("list", help="Lists tasks present in the application.")
+        list_task.add_argument(dest="list", type= str, metavar="LIST", help= "Lists the tasks",
+                               choices=["all", "in-progress", "done", "not-done"])
 
-update_task = subparser.add_parser("update", help="Updates the progress of an already added task")
-delete_task = subparser.add_parser("delete", help="Delete completed or unwanted tasks")
+        #done:update task using id and update status and update_date
+        update_task = self.subparser.add_parser("update", help="Updates the progress of an already added task")
+        update_task.add_argument(dest="id", type= str, metavar= "ID", help= "ID of task to be updated")
+        update_task.add_argument(dest="update", type= str, metavar= "UPDATE", help= "Update the description of task")
 
-args = parser.parse_args()
+        #done:Marking a task as in-progress or done
+        mark_task = self.subparser.add_parser("mark", help= "Mark the status of a particular task using id")
+        mark_task.add_argument(dest="id", type= str, metavar= "ID", help= "ID of task for which status is to be updated")
+        mark_task.add_argument(dest="status", type= str, metavar= "STATUS", help= "Status to be updated for the task",
+                               choices=["all", "in-progress", "done"])
 
-if args.option == "add":
-    add_tasks(args.task, args.status)
-if args.option == "list":
-    list_tasks(args.list)
+        #done:Delete task using id
+        delete_task = self.subparser.add_parser("delete", help="Delete completed or unwanted tasks")
+        delete_task.add_argument(dest="id", type= str, metavar= "ID", help= "ID of task to be deleted")
 
+        args = self.parser.parse_args()
 
-#Todo:update task using id and update status and update_date
+        if args.option == "add":
+            add_tasks(args.task, args.status)
+        elif args.option == "list":
+            list_tasks(args.list)
+        elif args.option == "update":
+            update_or_delete_task(task_id= args.id, desc= args.update)
+        elif args.option == "mark":
+            update_or_delete_task(task_id= args.id, status= args.status)
+        elif args.option == "delete":
+            update_or_delete_task(task_id= args.id)
 
-#Todo:List task that are done / not done/ in-progress
-
-#Todo: Change print statements to loggers
+if __name__ == "__main__":
+    to_do_app = Main()
+    to_do_app.start_app()
--- backend/to_do_helper.py
+++ backend/to_do_helper.py
@@ -1,57 +1,120 @@
 import json
 from datetime import datetime as dt
+import logging
 
 #initialize git -- Done
 #Add first task with id, desc, status, create_date, update_date
+#done:Add docstring for functions
+#Todo: Change print statements to loggers
+logger = logging.getLogger(name='__name__')
+logging.basicConfig(format= "%(levelname)s:%(message)s", level= logging.DEBUG)
+
 def add_tasks(task:str, status:str):
+    """
+    Function that handles addition of task to the cli app
+    :param task: Accepts string input of task description
+    :param status: OPTIONAL. Argument for status of task defaults to "To-do" status
+    :return: None
+    """
     try:
         with open("task_tacker.json", "r") as file:
             data = json.load(file)
-            data["count"] += 1
-            data["total_tasks"][data["count"]] = {
-                "task": task,
-                "status": status.title(),
-                "created_date": dt.now().strftime("%d-%m-%Y"),
-                "updated_date": dt.now().strftime("%d-%m-%Y")
-            }
+        data["count"] += 1
+        data["total_tasks"][data["count"]] = {
+            "task": task,
+            "status": status.title(),
+            "created_date": dt.now().strftime("%d-%m-%Y"),
+            "updated_date": dt.now().strftime("%d-%m-%Y")
+        }
 
     except FileNotFoundError as exp:
-            print(f"File Not present. Creating a new file")
-            data = {
-                "count": 1,
-                "total_tasks": {
-                    1: {
-                        "task": task,
-                        "status": status.title(),
-                        "created_date": dt.now().strftime("%d-%m-%Y"),
-                        "updated_date": dt.now().strftime("%d-%m-%Y")
-                    }
-
+        logger.warning(f"File Not present. Creating a new file")
+        data = {
+            "count": 1,
+            "total_tasks": {
+                1: {
+                    "task": task,
+                    "status": status.title(),
+                    "created_date": dt.now().strftime("%d-%m-%Y"),
+                    "updated_date": dt.now().strftime("%d-%m-%Y")
                 }
+
             }
+        }
 
     with open("task_tacker.json", "w") as file:
         json.dump(data, file, indent=4)
 
-    print (f"Task: {task} added to your task list | Status: {status.title()} | ID: {data["count"]}")
+    logger.info(f"Task: {task} added to your task list | Status: {status.title()} | ID: {data["count"]}")
 
 def print_list(index, task):
-    print(f"Task.{index + 1}: {task["task"]} | Status: {task["status"]} | Created Date: {task["created_date"]} | Last Updated: {task["updated_date"]}")
+    """
+    Prints the list of tasks from json file iteration
+    :param index: Id of task
+    :param task: description of task to be printed
+    :return: None
+    """
+    logger.info(f"Task.{index + 1}: {task["task"]} | Status: {task["status"]} | Created Date: {task["created_date"]} | Last Updated: {task["updated_date"]}")
 
 def list_tasks(option):
+    """
+    Lists the tasks already added by user
+    :param option: Accepts string values of all, done, in-progress and not-done options to be listed
+    :return: None
+    """
     try:
         with open("task_tacker.json", "r") as file:
             data = json.load(file)
 
-            for index,task in enumerate(data["total_tasks"].values()):
-                if task["status"].lower() == "in-progress" and  option.lower() == "in-progress":
+        for index,task in enumerate(data["total_tasks"].values()):
+            if task["status"].lower() == "in-progress" and  option.lower() == "in-progress":
+                print_list(index, task)
+            elif task["status"].lower() == "done" == option.lower():
+                print_list(index, task)
+            elif option.lower() == "not-done":
+                if task["status"].lower() != "done":
                     print_list(index, task)
-                elif task["status"].lower() == "done" == option.lower():
-                    print_list(index, task)
-                elif option.lower() == "not-done":
-                    if task["status"].lower() != "done":
-                        print_list(index, task)
-                elif option.lower == "all":
-                    print_list(index, task)
+            elif option.lower() == "all":
+                print_list(index, task)
     except FileNotFoundError as fne:
-        print("No Tasks found")
+        logger.warning("No Tasks found")
+
+def update_or_delete_task(task_id, desc=None, status = None):
+    """
+    Function to update task description or status or delete task based on the provided id
+    :param task_id: String value of task id should be passed.
+    :param desc: To update description of the task this parameter should be passed.
+    :param status: To update status of the task this parameter should be passed.
+    :return: None
+    """
+    data = {}
+    try:
+        with open("task_tacker.json", "r") as file:
+            data= json.load(file)
+    except FileNotFoundError as fne:
+        logger.warning("No task has been added yet. Please add task to proceed")
+
+    if desc:
+        if data:
+            data["total_tasks"][task_id]["task"] = desc
+            data["total_tasks"][task_id]["updated_date"] = dt.now().strftime("%d-%m-%Y")
+            with open("task_tacker.json", "w") as file_w:
+                json.dump(data, file_w, indent=4)
+            logger.info(f"Task: {task_id} updated!")
+    elif status:
+        if data:
+            data["total_tasks"][task_id]["status"] = status
+            with open("task_tacker.json", "w") as file_w:
+                json.dump(data, file_w, indent=4)
+            logger.info(f"Status of Task: {task_id} updated!")
+    else:
+        if data:
+            del data["total_tasks"][task_id]
+            with open("task_tacker.json", "w") as file_w:
+                json.dump(data, file_w, indent=4)
+            logger.info(f"Task: {task_id} deleted!")
+
+
+
+
+
